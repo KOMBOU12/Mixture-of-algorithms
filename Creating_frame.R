ea1c58377071831abce6bb9aa2660b69f9f2ff90
KOMBOU Jean Marius <jeankomboum@gmail.com>
2023-09-12 09:36:45 +0200

    Creating a frame with R

--- Creating_frame.R
+++ Creating_frame.R
@@ -16,3 +16,17 @@ rings <- c(FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE)
 # Create a data frame from the vectors
 planets_df <- data.frame(name, type, diameter, rotation, rings)
 planets_df
+# planets_df is pre-loaded in your workspace
+planets_df$rings
+# Select the rings variable from planets_df
+rings_vector <- planets_df$rings
+# Adapt the code to select all columns for planets with rings
+planets_df[rings_vector, ]
+# Select planets with diameter < 1
+subset(planets_df, subset = diameter < 1)
+
+# Use order() to create positions
+positions <-  order(planets_df$diameter)
+
+# Use positions to sort planets_df
+planets_df[positions,]

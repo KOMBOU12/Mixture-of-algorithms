af2d25fc150f102ec2270814f37e1076d2b1b135
KOMBOU Jean Marius <jeankomboum@gmail.com>
2023-09-14 08:52:15 +0200

    Departed List data frame

---
+++ Departed_list.R
@@ -0,0 +1,5 @@
+#Departed list
+scores <- c(4.6, 5, 4.8, 5, 4.2)
+comments <- c("I would watch it again", "Amazing!", "I liked it", "One of the best movies", "Fascinating plot")
+#Average of the score
+avg_scores <- mean(scores)

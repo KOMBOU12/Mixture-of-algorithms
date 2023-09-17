108eacd8989a1aae517dfaf57cb6cb337978398a
KOMBOU Jean Marius <jeankomboum@gmail.com>
2023-09-17 14:25:33 +0200

    Interpret

---
+++ Interpret.R
@@ -0,0 +1,19 @@
+# Linkedin and facebook 
+linkedin <- c(16, 9, 13, 5, 2, 17, 14 )
+facebook <- c( 17,  7,  5, 16,  8, 13, 14)
+
+# Define the interpret function
+interpret <- function(num_views) {
+  if (num_views > 15) {
+     print("You're popular!")
+     return(num_views)
+  } else {
+     print("Try to be more visible!")
+     return(0)
+
+  }
+}
+
+# Call the interpret function twice
+interpret(linkedin)
+interpret(facebook)6

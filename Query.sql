063f9c634a450f8436232fdae19600f626822517
KOMBOU Jean Marius <jeankomboum@gmail.com>
2023-10-07 11:19:00 +0200

    SQL code

---
+++ Query.sql
@@ -0,0 +1,7 @@
+-- !preview conn=DBI::dbConnect(RSQLite::SQLite
+-- Count the number of records in the people table
+SELECT COUNT (*) AS count_records
+FROM people ;
+-- Return the unique countries from the films table
+SELECT DISTINCT country 
+FROM films ;

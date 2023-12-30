b0c56d3aea757df51ab68f9a0a30afb20aa1f96d
KOMBOU Jean Marius <jeankomboum@gmail.com>
2023-12-30 23:03:46 +0100

    Confusion & Cross validation

--- Cross_Validation.R
+++ Cross_Validation.R
@@ -27,3 +27,19 @@ model <- train(
 
 # Print model to console
 print(model)
+
+# Fit lm model using 5 x 5-fold CV: model
+model <- train(
+  medv ~ ., 
+  Boston,
+  method = "lm",
+  trControl = trainControl(
+    method = "repeatedcv", 
+    number = 5,
+    repeats = 5, 
+    verboseIter = TRUE
+  )
+)
+
+# Print model to console
+print(model)

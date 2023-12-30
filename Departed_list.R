3ae3e19f0488f9adefa51b5d51f5cd5343666d2d
KOMBOU Jean Marius <jeankomboum@gmail.com>
2023-12-30 23:11:35 +0100

    Dep

--- Departed_list.R
+++ Departed_list.R
@@ -2,4 +2,6 @@
 scores <- c(4.6, 5, 4.8, 5, 4.2)
 comments <- c("I would watch it again", "Amazing!", "I liked it", "One of the best movies", "Fascinating plot")
 #Average of the score
-avg_scores <- mean(scores)
+avg_review <- mean(scores)
+reviews_df <- data.frame(scores, comments)
+reviews_df
